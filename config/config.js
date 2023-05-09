791ba01fcde2e05772555f70cd39fbde350bcf0e
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-05-09 14:19:11 +0800

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const {
+  DB_HOSTNAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+  DB_DIALECT,
+} = process.env; 
+
+module.exports = {
+  "development": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT,
+    "port" : DB_PORT,
+  },
+  "test": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT,
+    "port" : DB_PORT,
+  },
+  "production": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT,
+    "port" : DB_PORT,
+  }
+}
